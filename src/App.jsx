47320528c96a6e6bc1a92e240eb6b3948780943f
Stefan <stefanstevanovic019@gmail.com>
2024-10-21 11:02:45 +0200

    Added Lander

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
 import Cards from "./Cards.jsx";
+import Lander from "./Lander.jsx";
 function App() {
 
   return(
     <>
       <Header/> 
+      <Lander/>
       <Cards/>
       <Footer/>
     </>
---
+++ src/Lander.jsx
@@ -0,0 +1,13 @@
+function Lander(){
+    return(
+        <div className="lander-container">
+            <h1>Complete guide to mastering your skills</h1>
+            <h2>Become an expert in the field of computer science</h2>
+            <div id="lander-btns">
+            <button className="lander-btn" id="preview-btn">Course Preview</button>
+            <button className="lander-btn" id="buy-btn">Buy now!</button>
+            </div>
+        </div>
+    );
+}
+export default Lander
