c940927c5ba00f68087eb4fb66a125c105e5594f
Stefan <stefanstevanovic019@gmail.com>
2024-10-22 22:05:39 +0200

    Added Services and some css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
 import Cards from "./Cards.jsx";
 import Lander from "./Lander.jsx";
+import Services from "./Services.jsx";
 function App() {
 
   return(
     <>
       <Header/> 
       <Lander/>
+      <Services/>
       <Cards/>
       <Footer/>
     </>
